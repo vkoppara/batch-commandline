cb78a48ddb5ff9500baab1bd564c0e7dc9bfeccf
Venkata <gangaa@gmail.com>
2024-07-10 09:07:04 +0530

    initial commit

---
+++ src/main/java/com/vk/batch/commandline/batch_commandline/BatchCommandlineApplication.java
@@ -0,0 +1,13 @@
+package com.vk.batch.commandline.batch_commandline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCommandlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCommandlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/batch/commandline/batch_commandline/BatchCommandlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.batch.commandline.batch_commandline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCommandlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
